c504a4173761cc5970b7e878d277e2bfd6b5d33a
daisuke-tanabe <dev.daisuke.tanabe@gmail.com>
2025-03-15 04:30:57 +0900

    chore: import/orderを有効にするためeslintのプラグインを追加する

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -1,6 +1,7 @@
 import { FlatCompat } from '@eslint/eslintrc';
 import eslint from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
 import tseslint from 'typescript-eslint';
 import typeScriptESLintParser from '@typescript-eslint/parser';
 import { dirname } from 'path';
@@ -31,6 +32,7 @@ export default tseslint.config(
       ...compat.extends('next/core-web-vitals', 'next/typescript'),
     ],
     plugins: {
+      import: importPlugin,
       turbo: turboPlugin,
     },
     rules: {
--- packages/eslint-config/react-internal.js
+++ packages/eslint-config/react-internal.js
@@ -2,6 +2,7 @@ import eslint from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import reactPlugin from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
+import importPlugin from 'eslint-plugin-import';
 import typeScriptESLintParser from '@typescript-eslint/parser';
 
 import tseslint from 'typescript-eslint';
@@ -32,6 +33,7 @@ export default tseslint.config(
   },
   {
     plugins: {
+      import: importPlugin,
       'react-hooks': reactHooks,
       turbo: turboPlugin,
     },
