73dec8d27d800ed772c98070e17ddde97e510517
daisuke-tanabe <dev.daisuke.tanabe@gmail.com>
2025-04-07 21:11:24 +0900

    clean: 投稿の表示機能を削除する

--- apps/web/src/app/note/page.tsx
+++ apps/web/src/app/note/page.tsx
@@ -1,20 +1,6 @@
-import fs from 'node:fs';
-import path from 'path';
-
-import { format } from '@formkit/tempo';
 import { Metadata } from 'next';
 
-import { BlockSection, BlockSectionGroup, BlockSectionTitle, EntryList, EntryListItem } from '@/app/_components';
-
-type Post = {
-  title: string;
-  description: string;
-  pathname: string;
-  slug: string;
-  created_at: string;
-  updated_at: string;
-  deleted_at: string;
-};
+import { BlockSection, BlockSectionGroup, BlockSectionTitle, LeadText } from '@/app/_components';
 
 export const metadata: Metadata = {
   title: 'Posts',
@@ -22,29 +8,11 @@ export const metadata: Metadata = {
 };
 
 export default function PostsPage() {
-  const filePath = path.join(process.cwd(), 'docs/json/data.json');
-  const result = fs.readFileSync(filePath, 'utf-8');
-  const data = JSON.parse(result) as Post[];
-
   return (
     <BlockSectionGroup>
       <BlockSection>
-        <BlockSectionTitle>Note</BlockSectionTitle>
-        <EntryList>
-          {data.map(({ created_at: createdAt, description, pathname, title }, index) => (
-            <EntryListItem
-              key={index}
-              description={description}
-              label={format({
-                date: createdAt,
-                format: 'MMM DD',
-                tz: 'Asia/Tokyo',
-              })}
-              link={pathname}
-              title={title}
-            />
-          ))}
-        </EntryList>
+        <BlockSectionTitle>2025</BlockSectionTitle>
+        <LeadText>投稿が存在しません</LeadText>
       </BlockSection>
     </BlockSectionGroup>
   );
