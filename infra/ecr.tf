6c629ce685e02c83a76685c91e861bf73c8d9339
daisuke-tanabe <dev.daisuke.tanabe@gmail.com>
2025-12-06 08:25:19 +0900

    chore(infra): ECRライフサイクルポリシーを追加
    最新3イメージを保持し、古いイメージを自動削除

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -10,3 +10,22 @@ resource "aws_ecr_repository" "daisuke_tanabe_web_ecr" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "cleanup" {
+  repository = aws_ecr_repository.daisuke_tanabe_web_ecr.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "Keep last 3 images"
+      selection = {
+        tagStatus   = "any"
+        countType   = "imageCountMoreThan"
+        countNumber = 3
+      }
+      action = {
+        type = "expire"
+      }
+    }]
+  })
+}
